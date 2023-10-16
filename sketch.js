d3d47498ad411767bd1d09c78701dd8708b0839b
Githyhub <102429804+Githyhub@users.noreply.github.com>
2023-10-16 15:46:36 -0700

    WhenTypeNoWork

--- sketch.js
+++ sketch.js
@@ -1,29 +1,33 @@
 
-let textWordBlankHidden = 'hibus';
+let arrayWord = ['hello','mother','donde','slay'];
+let textWordBlankHidden = 'TEMP';
 let tempWord = ' ';
+let health = 3;
+
 
 for(let i = 0; i<textWordBlankHidden.length; i++){
   tempWord+= ' _ ';
  }
 
+
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
-
-  text("temp", displayWidth*0.45, displayHeight*0.45);
   let inputHuman = createInput('');
   inputHuman.input(refreshText);
   inputHuman.position(displayWidth*0.5, displayHeight*0.65);
+  
 }
 
 function refreshText(){
   clear();
-  tempWord= this.value()
-
-  if(tempWord.length>textWordBlankHidden.length){
+  tempWord= this.value();
+  if(tempWord.length>=textWordBlankHidden.length){
     tempWord=tempWord.substring(0,textWordBlankHidden.length);
   }
   
 }
+
 
 
 function draw() {
@@ -33,8 +37,25 @@ function draw() {
   text(tempWord, displayWidth*0.5, displayHeight*.9);
   textSize(30);
 
+  displayhealth();
+
+
+
+
 
   
-  
+}
+
+function displayhealth(){
+  let tempHeight = displayHeight*0.2;
+  let tempWidth = displayWidth*0.1;
+  let stringHearttemp = '';
+
+  while(health>0){
+    stringHearttemp+= ' heart ';
+    health = health -1;
+  }
+  text(stringHearttemp, tempHeight, tempWidth);
+  textStyle(Bold);
   
 }
